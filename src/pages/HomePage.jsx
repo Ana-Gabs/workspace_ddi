62554a80e18f4a3bfc773ac137a7b921b26f373f
Ana-Gabs <2022371069@uteq.edu.mx>
2025-05-21 01:51:31 -0600

    "cambio a estructura modular"

--- src/pages/HomePage.jsx
+++
@@ -1,8 +0,0 @@
-// src/pages/HomePage.jsx
-import React from 'react';
-
-function HomePage() {
-  return <h1>Bienvenido a la Página Principal</h1>;
-}
-
-export default HomePage;
--- src/pages/NotFound.jsx
+++
@@ -1,8 +0,0 @@
-// src/pages/NotFound.jsx
-import React from 'react';
-
-function NotFound() {
-  return <h1>404 - Página no encontrada</h1>;
-}
-
-export default NotFound;
